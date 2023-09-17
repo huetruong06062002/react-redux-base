41a92940ffce09a2114920367677a4437ae12c9b
huetruong06062002 <truongvshieu01@gmail.com>
2023-09-17 21:23:33 +0700

    vidoe 23

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,11 +1,15 @@
 import React, { Component } from "react";
 import UserInfor from "./UserInfor";
+import DisplayInfor from "./DisplayInfor";
 
 export default class MyComponent extends Component {
   render() {
     return (
       <div>
         <UserInfor />
+        <DisplayInfor name="Hoi dan it" age={28} />
+        <hr />
+        <DisplayInfor name="Eric" age={29} />
       </div>
     );
   }
