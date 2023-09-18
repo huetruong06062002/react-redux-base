e19df4e82f6d88fc4b3a68c00d004fd63e1cd8d4
huetruong06062002 <truongvshieu01@gmail.com>
2023-09-18 18:18:23 +0700

    video 27

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import "./DisplayInfor.scss";
 
 export default class DisplayInfor extends Component {
   state = {
@@ -18,14 +19,16 @@ export default class DisplayInfor extends Component {
             {this.state.isShowHide ? "Hide list users" : "Show list users"}
           </button>
         </div>
-        {this.state.isShowHide &&
-          listUsers.map((user) => (
-            <div key={user.id} className={~~user.age <= 18 ? "red" : "green"}>
-              <p>Name: {user.name} </p>
-              <p>Age: {user.age} </p>
-              <hr />
-            </div>
-          ))}
+        <div className="display-infor-container">
+          {this.state.isShowHide &&
+            listUsers.map((user) => (
+              <div key={user.id} className={~~user.age <= 18 ? "red" : "green"}>
+                <p>Name: {user.name} </p>
+                <p>Age: {user.age} </p>
+                <hr />
+              </div>
+            ))}
+        </div>
       </>
     );
   }
